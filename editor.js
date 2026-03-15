9923d9c7c6a2c07916cb4af6964d8fb310f57b8a
SyedUmar286 <nizaliadilshad16@gmail.com>
2026-03-15 07:35:05 +0400

    Create editor.js

---
+++ editor.js
@@ -0,0 +1,8 @@
+console.log("Editor loaded!");
+
+// Yeh function images upload karne ke liye hai
+function handleImageUpload(event) {
+    const files = event.target.files;
+    console.log("Images selected:", files);
+    // Yahan hum baad mein timeline mein image dikhane ka code likhenge
+}
